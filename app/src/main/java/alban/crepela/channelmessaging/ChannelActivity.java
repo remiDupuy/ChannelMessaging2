3e4144911a28e62703e7a586abbef400aabf7d04
remiDupuy <remi.dup73@gmail.com>
2017-01-23 11:07:57 +0100

    v1.2

--- app/src/main/java/alban/crepela/channelmessaging/ChannelActivity.java
+++ app/src/main/java/alban/crepela/channelmessaging/ChannelActivity.java
@@ -4,6 +4,9 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.gson.Gson;
@@ -14,6 +17,8 @@ import java.util.HashMap;
 public class ChannelActivity extends AppCompatActivity {
 
     private ListView messages;
+    private Button btnSend;
+    private EditText txtSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,7 @@ public class ChannelActivity extends AppCompatActivity {
 
         final Runnable r = new Runnable() {
             public void run() {
-                System.out.println("test");
+
                 messages = (ListView) findViewById(R.id.listViewMessages);
 
                 SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
@@ -49,6 +54,33 @@ public class ChannelActivity extends AppCompatActivity {
                         messages.setAdapter(adapter);
                     }
                 });
+
+
+
+                btnSend = (Button)findViewById(R.id.btnSend);
+                txtSend = (EditText)findViewById(R.id.txtSend);
+                btnSend.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
+                        String accesstoken = settings.getString("accesstoken","");
+
+                        HashMap<String, String> params = new HashMap<String, String>();
+                        params.put("accesstoken",accesstoken);
+                        params.put("channelid", getIntent().getStringExtra("channelID"));
+                        params.put("message", txtSend.getText().toString());
+
+                        Connexion connexion = new Connexion(getApplicationContext(), params, "http://www.raphaelbischof.fr/messaging/?function=sendmessage");
+                        connexion.execute();
+
+                        txtSend.setText("");
+
+                    }
+                });
+
+
+
                 connexion.execute();
 
 
